ccc80072817555ee010fd1839940c9fec58c5a92
Margot Geerts <44675304+margotgeerts@users.noreply.github.com>
2024-09-15 21:00:26 +0200

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -18,5 +18,30 @@ var addressPoints = [
     "Los Angeles, CA",
     34.0543942,
     -118.2439408
-  ]
+  ],
+  [
+    "Copenhagen, Denmark",
+    55.676098,
+    12.568337
+  ],
+  [
+    "Helsinki, Finland",
+    60.192059,
+    24.945831
+  ],
+  [
+    "Antwerp, Belgium",
+    51.260197,
+    4.402771
+  ],
+  [
+    "Macao, SAR China",
+    22.210928, 
+    113.552971
+  ],
+  [
+    "Jeju, South Korea",
+    33.4890113,
+    126.4983023
+  ],
 ];
